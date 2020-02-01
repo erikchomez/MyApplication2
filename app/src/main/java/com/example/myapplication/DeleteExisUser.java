b1afe3210007c0b556fb9c66a809b5a88131d8fd
nkachami <nkachami@users.noreply.github.com>
2020-02-01 11:18:27 -0800

    Update DeleteExisUser.java
    Imports added

--- app/src/main/java/com/example/myapplication/DeleteExisUser.java
+++ app/src/main/java/com/example/myapplication/DeleteExisUser.java
@@ -2,5 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 public class DeleteExisUser extends AppCompatActivity {
+    /**
+     ** Activity where the user deletes people from a group.
+     */
+    
 }
