51312b316d77c3c4f2dc29b644e564c78f180015
mgazich <50192506+mgazich@users.noreply.github.com>
2020-02-02 08:04:36 -0800

    Updated CreateNewUser.java
    Implemented an attendance array for individual objects of the CreateNewUser class

--- app/src/main/java/com/example/myapplication/CreateNewUser.java
+++ app/src/main/java/com/example/myapplication/CreateNewUser.java
@@ -1,17 +1,33 @@
 package com.example.myapplication;
 
+import java.util.ArrayList;
+
 public class CreateNewUser {
     private String mName;
     private boolean mWorkedOut;
     private int mPasses;
+    private ArrayList<Integer> attendanceArray;
 
     public CreateNewUser(String name) {
         this.mName = name;
         this.mWorkedOut = false;
         this.mPasses = 2;
+        this.attendanceArray = new ArrayList<Integer>();
+        for(int i = 0; i < 6; i++){
+            this.attendanceArray.add(0);
+        }
+
     }
 
     public String getName() {
         return this.mName;
+    }
+
+    public ArrayList<Integer> getAttendanceArray(){
+        return attendanceArray;
+    }
+
+    public void setAttendenceArray(int num, int index){
+        attendanceArray.set(index, num);
     }
 }
